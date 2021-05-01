2b0dd9fcd01621d6d81040efe080f7c769a33cae
Rasmiranjan <nayakrasmiranjan929@gmail.com>
2021-05-01 07:29:18 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#Write a python program to store student details (student id, student name, student location) and display back the details.
+stid=1821106117
+stname="Rasmiranjan"
+stloc='Bhubaneswar'
+print("Student ID is:",stid)
+print("Student Name is:",stname)
+print("Student Location is:",stloc)
